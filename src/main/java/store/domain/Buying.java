289c6195e95481ab8b05c4071a5d16f7433d8cc6
이윤빈 <kanado20000@naver.com>
2024-11-07 22:14:49 +0900

    :sparkles: feat: 편의점 구매 사이클을 관리하기 위한 Buying 클래스 추가

---
+++ src/main/java/store/domain/Buying.java
@@ -0,0 +1,21 @@
+package store.domain;
+
+public class Buying {
+    private BuyingStatus status;
+
+    private Buying(BuyingStatus status) {
+        this.status = status;
+    }
+
+    public static Buying startBuying() {
+        return new Buying(BuyingStatus.BUYING);
+    }
+
+    public void endBuying() {
+        this.status = BuyingStatus.FINISH;
+    }
+
+    public boolean isEnd() {
+        return this.status == BuyingStatus.FINISH;
+    }
+}
