c2a22f63241e4b4d9d666b24b3826cf1dafe639d
이윤빈 <kanado20000@naver.com>
2024-11-07 14:07:30 +0900

    :sparkles: feat: 상품 파일을 관리하는 ProductFileHandler 클래스 추가
    readFileAsString 메서드는 상품 객체를 만들기 위해 파일 내용을 문자열로 반환해주고,
    showProductsToUser 메서드는 사용자에서 재고 상태를 보여주기 위해 필요한 작업
    후에 문자열로 반환한다.

---
+++ src/main/java/store/enums/ErrorMessage.java
@@ -0,0 +1,17 @@
+package store.enums;
+
+public enum ErrorMessage implements SystemMessage {
+    PROBLEM_WITH_FILE("파일을 읽는 중 문제가 발생했습니다. 파일 경로와 파일 상태를 확인한 후 다시 시도해 주세요.");
+
+    private static final String ERROR_LOG_LEVEL = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return ERROR_LOG_LEVEL + message;
+    }
+}
---
+++ src/main/java/store/enums/SystemMessage.java
@@ -0,0 +1,5 @@
+package store.enums;
+
+public interface SystemMessage {
+    String getMessage();
+}
