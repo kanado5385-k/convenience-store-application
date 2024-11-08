ee23a517910f622d407a753b94a1cfe6a9a81734
이윤빈 <kanado20000@naver.com>
2024-11-08 16:22:58 +0900

    :sparkles: feat: Inventory 클래스에서 제품이 유효한 프로모션을 가지고 있는지 확인하는isProductWithPromotion 메서드 추가

--- src/main/java/store/domain/Inventory.java
+++ src/main/java/store/domain/Inventory.java
@@ -1,6 +1,7 @@
 package store.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Inventory {
     private final List<Product> products;
@@ -10,5 +11,28 @@ public class Inventory {
         this.products = products;
         this.promotions = promotions;
     }
+
+    public boolean isProductWithPromotion(String productName){
+        List<Product> products = this.products.stream()
+            .filter(Product -> Product.isSameName(productName))
+            .filter(Product -> Product.hasPromotion())
+            .collect(Collectors.toList());
+        if (products.isEmpty()){
+            return false;
+        }
+
+        return isValidDateOfPromotion(products);      
+    }
+
+    private boolean isValidDateOfPromotion(List<Product> products){
+        Product product = products.get(0);
+        String promotionName = product.getPromotionBoon();
+        List<Promotion> promotions = this.promotions.stream()
+            .filter(Promotion -> Promotion.isSamePromotionName(promotionName))
+            .collect(Collectors.toList());
+        Promotion promotion = promotions.get(0);
+
+        return promotion.isBetweenStartAndEndDate();
+    } 
 
 }
