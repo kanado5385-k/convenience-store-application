8c59f7bc3139b903dfca68c81899b909aae44924
이윤빈 <kanado20000@naver.com>
2024-11-09 23:40:00 +0900

    :sparkles: feat: 사용자 입력을 받기 위한 InputView 클래스 추가
    readOrder 메서드는 주문을 입력 받고, readMemberShipOrNot 메서드는 멤버십할인 여부를 입력 받고,
    readAdditionalOrderOrNot 메서드는 추자 주문 여부를 입력 받기위한 것이다.

--- src/main/java/store/view/input/InputView.java
+++ src/main/java/store/view/input/InputView.java
@@ -1,5 +1,27 @@
 package store.view.input;
 
+import camp.nextstep.edu.missionutils.Console;
+import store.enums.messages.IOMessage;
+
 public class InputView {
-    
+    public String readOrder() {
+        System.out.println(String.format(IOMessage.INPUT_ORDER.getMessage()));
+        String answer = Console.readLine();
+
+        return answer.replace(" ", "");
+    }
+
+    public String readMemberShipOrNot() {
+        System.out.println(String.format(IOMessage.INPUT_MEMBER_SHIP.getMessage()));
+        String answer = Console.readLine();
+
+        return answer.replace(" ", "");
+    }
+
+    public String readAdditionalOrderOrNot() {
+        System.out.println(String.format(IOMessage.INPUT_ADDITIONAL_ORDER.getMessage()));
+        String answer = Console.readLine();
+
+        return answer.replace(" ", "");
+    }
 }
