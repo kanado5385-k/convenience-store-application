642b26af29592f4222f701d4649863986bc00a33
이윤빈 <kanado20000@naver.com>
2024-11-09 21:49:28 +0900

    :sparkles: feat: 영수증을 정보를 전달하기 위한 ReseiptDTO 클래스 추가

---
+++ src/main/java/store/dto/ReseiptDTO.java
@@ -0,0 +1,52 @@
+package store.dto;
+
+import store.domain.Product;
+
+import java.util.List;
+import java.util.Map;
+
+public class ReseiptDTO {
+
+    private Map<String, Integer> promotionProducts; 
+    private List<Product> boughtProducts;           
+    private Integer memberShipDiscount;             
+    private Integer promotionDiscount;              
+    private Integer generalPrice;                  
+    private Integer totalPrice;                     
+
+    public ReseiptDTO(Map<String, Integer> promotionProducts, List<Product> boughtProducts,
+                      Integer memberShipDiscount, Integer promotionDiscount,
+                      Integer generalPrice, Integer totalPrice) {
+        this.promotionProducts = promotionProducts;
+        this.boughtProducts = boughtProducts;
+        this.memberShipDiscount = memberShipDiscount;
+        this.promotionDiscount = promotionDiscount;
+        this.generalPrice = generalPrice;
+        this.totalPrice = totalPrice;
+    }
+
+    public Map<String, Integer> getPromotionProducts() {
+        return promotionProducts;
+    }
+
+    public List<Product> getBoughtProducts() {
+        return boughtProducts;
+    }
+
+    public Integer getMemberShipDiscount() {
+        return memberShipDiscount;
+    }
+
+    public Integer getPromotionDiscount() {
+        return promotionDiscount;
+    }
+
+    public Integer getGeneralPrice() {
+        return generalPrice;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice;
+    }
+
+}
