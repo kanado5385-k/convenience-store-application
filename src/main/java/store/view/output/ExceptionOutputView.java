aa97e91a30a0663104a8bde4c158380d9e0d7a97
이윤빈 <kanado20000@naver.com>
2024-11-10 17:28:04 +0900

    :sparkles: feat: error 메세지를 출력하는 ExceptionOutputView 유틸리티 클래스 추가

---
+++ src/main/java/store/view/output/ExceptionOutputView.java
@@ -0,0 +1,8 @@
+package store.view.output;
+
+public class ExceptionOutputView {
+    
+    public static void printErrorMessage(String errorMessage){
+        System.out.println(errorMessage + System.lineSeparator());
+    }
+}
